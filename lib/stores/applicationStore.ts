ab37cf6de9f97e498f2df57be02a0ed1a7239311
ScuBix1 <bastian.monnin1@gmail.com>
2026-07-12 20:56:26 +0200

    feat(store): add follow-up filter to application store

--- lib/stores/applicationStore.ts
+++ lib/stores/applicationStore.ts
@@ -3,14 +3,18 @@ import { create } from 'zustand';
 
 type ApplicationStore = {
   applications: ApplicationCard[];
+  showFollowUpOnly: boolean;
   setApplications: (applications: ApplicationCard[]) => void;
   updateApplication: (updated: ApplicationCard) => void;
   removeApplication: (id: string) => void;
   moveApplication: (id: string, status: ApplicationCard['status']) => void;
+  toggleFollowUpFilter: () => void;
+  resetFollowUpFilter: () => void;
 };
 
 export const useApplicationStore = create<ApplicationStore>((set) => ({
   applications: [],
+  showFollowUpOnly: false,
 
   setApplications: (applications) => set({ applications }),
 
@@ -28,4 +32,8 @@ export const useApplicationStore = create<ApplicationStore>((set) => ({
     set((state) => ({
       applications: state.applications.map((app) => (app.id === id ? { ...app, status } : app)),
     })),
+
+  toggleFollowUpFilter: () => set((state) => ({ showFollowUpOnly: !state.showFollowUpOnly })),
+
+  resetFollowUpFilter: () => set({ showFollowUpOnly: false }),
 }));
