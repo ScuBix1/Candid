5eb12118079524d6d2db5d4ad41f122f9fa1cb25
ScuBix1 <bastian.monnin1@gmail.com>
2026-07-14 22:45:48 +0200

    feat(landing): add SEO metadata, sitemap and robots

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import CallToAction from '@/components/landing/CallToAction/CallToAction';
+import Features from '@/components/landing/Features/Features';
+import Footer from '@/components/landing/Footer/Footer';
+import Header from '@/components/landing/Header/Header';
+import Hero from '@/components/landing/Hero/Hero';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Features />
+        <CallToAction />
+      </main>
+      <Footer />
+    </>
+  );
 }
