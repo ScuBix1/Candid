b5f479042558e348a81690548e8a8460c9aae43d
ScuBix1 <bastian.monnin1@gmail.com>
2026-07-14 11:07:57 +0200

    fix(application): remove console.log from onSubmit

--- components/dashboard/Application/ApplicationForm/ApplicationForm.tsx
+++ components/dashboard/Application/ApplicationForm/ApplicationForm.tsx
@@ -56,7 +56,6 @@ export default function ApplicationForm({ onSuccess, application }: ApplicationF
   });
 
   async function onSubmit(values: CreateApplicationSchema) {
-    console.log('values', values);
     setIsLoading(true);
     setServerError(null);
 
